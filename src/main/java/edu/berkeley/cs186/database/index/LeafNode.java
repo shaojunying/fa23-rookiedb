c6a0cdf8fa43868757d22398dba6e0f3970571d0
shaojunying <shaojunying1@gmail.com>
2023-11-29 19:15:48 +0800

    完成了LeafNode的fromBytes方法

--- src/main/java/edu/berkeley/cs186/database/index/LeafNode.java
+++ src/main/java/edu/berkeley/cs186/database/index/LeafNode.java
@@ -372,12 +372,22 @@ class LeafNode extends BPlusNode {
      */
     public static LeafNode fromBytes(BPlusTreeMetadata metadata, BufferManager bufferManager,
                                      LockContext treeContext, long pageNum) {
-        // TODO(proj2): implement
-        // Note: LeafNode has two constructors. To implement fromBytes be sure to
-        // use the constructor that reuses an existing page instead of fetching a
-        // brand new one.
+        Page page = bufferManager.fetchPage(treeContext, pageNum);
+        Buffer buf = page.getBuffer();
+        byte nodeType = buf.get();
+        assert (byte) 1 == nodeType;
 
-        return null;
+        Optional<Long> rightSibling = Optional.of(buf.getLong());
+
+        int count = buf.getInt();
+        List<DataBox> keys = new ArrayList<>();
+        List<RecordId> rids = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            keys.add(DataBox.fromBytes(buf, metadata.getKeySchema()));
+            rids.add(RecordId.fromBytes(buf));
+        }
+        return new LeafNode(metadata, bufferManager, page, keys,
+                rids, rightSibling, treeContext);
     }
 
     // Builtins ////////////////////////////////////////////////////////////////
